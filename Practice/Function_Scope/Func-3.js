c47c0c7223a1c4d0be85a79c4706e0eee2ac50d8
parisatrisha <167854344+parisatrisha@users.noreply.github.com>
2025-02-13 14:56:08 +0530

    initial commit

---
+++ Practice/Function_Scope/Func-3.js
@@ -0,0 +1,10 @@
+function sports()
+{
+    if(10<20)
+    {
+        let cricket="virat"
+        document.write(cricket)
+    }
+    document.write(cricket)
+}
+sports()
---
+++ Practice/Function_Scope/func-2.js
@@ -0,0 +1,15 @@
+//Function Scope
+
+var Apple="red";
+let Orange="yellow";
+const banana="green";
+function fruit()
+{
+    var x=100;
+    var y=200;
+    const z="Mahesh";
+    document.write(x)
+    document.write(y)
+    document.write(z)
+}
+fruit()
